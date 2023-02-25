bbc09a15e620ea8e59b8b2ae418f1a702f9d2db7
Nikoloz97 <nick.gotsy@gmail.com>
2023-02-24 22:32:08 -0500

    Add project files.

---
+++ BiochemSite/Controllers/ContentController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BiochemSite.Controllers
+{
+    [ApiController]
+    public class ContentController : ControllerBase
+    {
+        public JsonResult GetChapterContent()
+        {
+            new JsonResult(
+                new List<object>
+                {
+                    new {id = 1, ChapterNum = 1, SubChapNum = 1 ChapDesc = "Introduction to essential Amino acids"},
+                    new {id = 2,ChapterNum = 1, SubChapNum = 2, ChapDesc = "Learning "},
+                    new {id = 3, ChapterNum = 2,SubChapNum = 1, ChapDesc = },
+                    new {id = 4, ChapterNum = 2,SubChapNum = 2, ChapDesc = }
+                })
+        }
+
+    }
+}
---
+++ BiochemSite/Controllers/StatusExamController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BiochemSite.Controllers
+{
+    public class StatusExamController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
